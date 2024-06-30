3530378660c94c0575e8c4d55af5e47ff53bfb65
Lasith Randima <lasithrandima123@gmail.com>
2024-06-30 17:22:48 +0530

    better comments ext add

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-//  To use ES6 features, you need to update your package.json file to include the following:
-// "type": "module"
-// To use ES2015 import syntax, you need to use Babel to transpile the code. 
+//!  To use ES6 features, you need to update your package.json file to include the following:
+//? "type": "module"
 
-// To use ES6 features, you should have to specify file extension in the import statement. otherwise it gives errors
-// importing modules not with default export
+//! To use ES6 features, you should have to specify file extension in the import statement. otherwise it gives errors
+//* importing modules not with default export
 // import { getPosts } from "./postController.js";
 
-// importing modules with default export
+//* importing modules with default export
 // import getPosts from "./postController.js";
 
-// importing multiple modules in same file with default export
+//* importing multiple modules in same file with default export
 import getPosts, { getPostsLength } from "./postController.js";
 
 
--- postController.js
+++ postController.js
@@ -18,13 +18,13 @@ const getPosts = () => posts;
 // exporting another function apart from default 
 export const getPostsLength = () => posts.length;
 
-// this is not default export. Should use curly braces while importing
+//* this is not default export. Should use curly braces while importing
 // export const getPosts = () => posts;
 
-// Another way to export. Should use curly braces while importing
+//* Another way to export. Should use curly braces while importing
 // export { getPosts };
 
 // Default export. Should use any name while importing without curly braces
-// With the default export, you can not only export one thing. For example, you can export an object, array, or another function etc.
+//* With the default export, you can not only export one thing. For example, you can export an object, array, or another function etc.
 // In here we are exporting a function called getPostsLength apart from the default export
 export default getPosts;
