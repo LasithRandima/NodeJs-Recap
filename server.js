c0788dd7836c3899449a39b8307c2dc83db18a0f
Lasith Randima <lasithrandima123@gmail.com>
2024-06-30 21:03:04 +0530

    Routes with request method checking

--- server.js
+++ server.js
@@ -6,17 +6,43 @@ const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
 
-//   Create simple router in node
-if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>HomePage</h1>');
-    } else if (req.url === '/about') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>About Page</h1>');
-    } else {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('<h1>Page Not Found</h1>');
+    try {
+        // Check if GET request
+        if (req.method === 'GET') {
+            // Check if request is for the homepage
+            if (req.url === '/') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end('<h1>HomePage</h1>');
+            }
+            // Check if request is for the about page
+            else if (req.url === '/about') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end('<h1>About Page</h1>');
+            }
+            // Check if request is for the 404 page
+            else {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>Page Not Found</h1>');
+            }
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Server Error: ' + error.message);
     }
+
+//   Create simple router in node
+// if (req.url === '/') {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end('<h1>HomePage</h1>');
+//     } else if (req.url === '/about') {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end('<h1>About Page</h1>');
+//     } else {
+//     res.writeHead(404, { 'Content-Type': 'text/html' });
+//     res.end('<h1>Page Not Found</h1>');
+//     }
 
 
 // using htp module we can access request and response objects and display them in browser
